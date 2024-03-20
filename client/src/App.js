748c432870d8be8c2aa8d8328f90ec22c7a90bd8
Zavier Andrianarivo <zoa215@nyu.edu>
2024-03-20 15:14:00 -0700

    installed cors dependency for reading client requests to express backend and express sending http responses back to client-side. also did some file restructuring for components and pages. will discuss creating private routing for secure access via login to the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('Hello, World!');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data = setMessage(data.message);
+      })
+      .catch((error) => {
+        console.log('error fetching', error)
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {message}
+      </h1>
     </div>
   );
 }
--- server/index.mjs
+++ server/index.mjs
@@ -1,12 +1,21 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
-	res.send('test express app');
+	console.log(req.query);
+	res.json({message: 'test d\'éclat app'});
 });
 
 app.listen(PORT, () => {
